a545221641b69598c3e660e6f19d324103541a77
hugob <100495717@alumnos.uc3m.es>
2024-03-13 17:37:17 +0100

    libreria

---
+++ src/main/python/test_hotel_manager_test.py
@@ -0,0 +1,12 @@
+import unittest
+from UC3MTravel.HotelManager import HotelManager
+class TestHotel(unittest.TestCase):
+    def test_room_reservation(self):
+        my_reservation = HotelManager()
+        value = my_reservation.room_reservation
+        self.assertEqual(value,
+
+
+    if __name__ == "__main__"):
+        main()
+
